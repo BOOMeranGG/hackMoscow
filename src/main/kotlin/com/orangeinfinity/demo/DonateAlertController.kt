3265a363692e17fffb36ba2427ce278cba0edb1e
Jenya Bykov <mothersasg@yandex.ru>
2019-10-26 18:21:32 +0300

    Creating invoice link

--- src/main/kotlin/com/orangeinfinity/demo/DonateAlertController.kt
+++ src/main/kotlin/com/orangeinfinity/demo/DonateAlertController.kt
@@ -2,6 +2,7 @@ package com.orangeinfinity.demo
 
 import com.orangeinfinity.demo.model.invoice.InvoiceCreateDto
 import com.orangeinfinity.demo.model.invoice.InvoiceResultDto
+import com.orangeinfinity.demo.model.invoice.LinkDto
 import org.apache.commons.logging.LogFactory
 import org.springframework.web.bind.annotation.*
 
@@ -12,14 +13,16 @@ class DonateAlertController(val plasmaApiService: PlasmaApiService) {
     private val log = LogFactory.getLog(javaClass)
     private val apiKey = "d16350f8-2597-42bf-b72a-a3baaeff11e3"
 
-    @PostMapping("/newDonation/create")
-    fun createInvoice(): String {
+    @CrossOrigin(origins = ["*"])
+    @PostMapping("/newDonation/confirm")
+    fun createInvoice(@RequestBody requestJson: String) {
+        log.info("Received webhook info: $requestJson")
 
-
-        return "It's work!"
+        println(4)
     }
 
-    @GetMapping("newDonation/get")
+    @CrossOrigin(origins = ["*"])
+    @PostMapping("newDonation/get")
     fun getNewDonation(@RequestBody requestJson: String): String? {
         val invoiceDto = JacksonUtils.serializingObjectMapper().readValue(requestJson, InvoiceCreateDto::class.java)
         val invoiceResponse = plasmaApiService.createInvoice(
@@ -31,7 +34,8 @@ class DonateAlertController(val plasmaApiService: PlasmaApiService) {
         log.info("Creating invoice ended. Result: $invoiceResponse")
         val invoiceResultDto = JacksonUtils.serializingObjectMapper()
                 .readValue(invoiceResponse, InvoiceResultDto::class.java)
+        val linkDto = LinkDto(invoiceResultDto.link)
 
-        return invoiceResultDto.link
+        return JacksonUtils.serializingObjectMapper().writeValueAsString(linkDto)
     }
 }
---
+++ src/main/kotlin/com/orangeinfinity/demo/model/invoice/LinkDto.kt
@@ -0,0 +1,3 @@
+package com.orangeinfinity.demo.model.invoice
+
+class LinkDto(val link: String)
